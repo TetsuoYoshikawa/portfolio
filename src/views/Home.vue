f38cf39ee47ef95f9ad6a6dc61c72154beb5b774
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-12-08 22:09:55 +0900

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default{
+  data(){
+    return{
+      portfolio:[],
+      skill:[]
+    }
+  },
+  methods:{
+    async getPortfolio(){
+      await axios
+        .get("http://127.0.0.1:8000/api/portfolio")
+        .then((response) => {
+          this.portfolio = response.data.data;
+        })
+    },
+    async getSkill(){
+      await axios
+        .get("http://127.0.0.1:8000/api/skill")
+        .then((response) => {
+          this.skill = response.data.data;
+        })
+    },
+  },
+  created(){
+    this.getPortfolio();
+    this.getSkill();    
+  }
+}
+</script>
